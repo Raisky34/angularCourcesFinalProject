ffa826b4ee5b832c88e0f06e9c9342c4c78b59fe
Raisky34 <raisky34@gmail.com>
2017-06-21 15:49:20 +0300

    Change creads for mine

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,10 @@
 var config = {
-    apiKey: "AIzaSyAa9MV3QqwpoWbMs--FdNBImtNlhgYcVrw",
-    authDomain: "chatapp-910bc.firebaseapp.com",
-    databaseURL: "https://chatapp-910bc.firebaseio.com",
-    storageBucket: "chatapp-910bc.appspot.com",
-    messagingSenderId: "220911885684"
+    apiKey: "AIzaSyAFvHfZ0j5GE_IylgK-MgxUCWx5V9ILArM",
+    authDomain: "fir-f30ba.firebaseapp.com",
+    databaseURL: "https://fir-f30ba.firebaseio.com",
+    projectId: "fir-f30ba",
+    storageBucket: "fir-f30ba.appspot.com",
+    messagingSenderId: "67694317702"
   };
   firebase.initializeApp(config);
 angular.module('chatApp', ['firebase'])
